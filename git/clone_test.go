d6372b752a5f0a16a7a7dcd44f5fe898ababe7a4
Robert Kaussow <mail@thegeeklab.de>
2024-05-06 16:42:12 +0200

    add more tests

--- git/clone_test.go
+++ git/clone_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFetch(t *testing.T) {
+func TestFetchSource(t *testing.T) {
 	testdata := []struct {
 		name  string
 		repo  *Repository
@@ -65,6 +65,142 @@ func TestFetch(t *testing.T) {
 	for _, tt := range testdata {
 		t.Run(tt.name, func(t *testing.T) {
 			cmd := tt.repo.FetchSource(tt.repo.CommitRef)
+			require.Equal(t, tt.want, cmd.Args)
+		})
+	}
+}
+
+func TestFetchTags(t *testing.T) {
+	testdata := []struct {
+		name  string
+		repo  *Repository
+		tags  bool
+		depth int
+		want  []string
+	}{
+		{
+			name: "fetch tags",
+			repo: &Repository{},
+			tags: true,
+			want: []string{
+				gitBin,
+				"fetch",
+				"--tags",
+				"--quiet",
+				"origin",
+			},
+		},
+	}
+
+	for _, tt := range testdata {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := tt.repo.FetchTags()
+			require.Equal(t, tt.want, cmd.Args)
+		})
+	}
+}
+
+func TestFetchLFS(t *testing.T) {
+	testdata := []struct {
+		name string
+		repo *Repository
+		want []string
+	}{
+		{
+			name: "fetch LFS",
+			repo: &Repository{},
+			want: []string{
+				gitBin,
+				"lfs",
+				"fetch",
+			},
+		},
+	}
+
+	for _, tt := range testdata {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := tt.repo.FetchLFS()
+			require.Equal(t, tt.want, cmd.Args)
+		})
+	}
+}
+
+func TestCheckoutHead(t *testing.T) {
+	testdata := []struct {
+		name string
+		repo *Repository
+		want []string
+	}{
+		{
+			name: "checkout head",
+			repo: &Repository{},
+			want: []string{
+				gitBin,
+				"checkout",
+				"--force",
+				"--quiet",
+				"FETCH_HEAD",
+			},
+		},
+	}
+
+	for _, tt := range testdata {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := tt.repo.CheckoutHead()
+			require.Equal(t, tt.want, cmd.Args)
+		})
+	}
+}
+
+func TestCheckoutSha(t *testing.T) {
+	testdata := []struct {
+		name string
+		repo *Repository
+		want []string
+	}{
+		{
+			name: "checkout sha",
+			repo: &Repository{
+				CommitSha: "abcd1234",
+			},
+			want: []string{
+				gitBin,
+				"reset",
+				"--hard",
+				"--quiet",
+				"abcd1234",
+			},
+		},
+	}
+
+	for _, tt := range testdata {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := tt.repo.CheckoutSha()
+			require.Equal(t, tt.want, cmd.Args)
+		})
+	}
+}
+
+func TestCheckoutLFS(t *testing.T) {
+	testdata := []struct {
+		name string
+		repo *Repository
+		want []string
+	}{
+		{
+			name: "checkout LFS with no arguments",
+			repo: &Repository{},
+			want: []string{
+				gitBin,
+				"lfs",
+				"checkout",
+			},
+		},
+	}
+
+	for _, tt := range testdata {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := tt.repo.CheckoutLFS()
 			require.Equal(t, tt.want, cmd.Args)
 		})
 	}
